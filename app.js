10e995c78d8b66805ae1194f773bc89f6b76d572
fanifrancs <fanikufran6@gmail.com>
2022-10-13 00:39:22 +0100

    'update'

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
-const express = require("express"),
+const express = require('express'),
 app = express(),
-ytdl = require("ytdl-core");
+ytdl = require('ytdl-core');
 
 app.use(express.static('public'));
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-	res.render("index");
+app.get('/', (req, res) => {
+	res.render('index');
 });
 
-app.get("/download", async(req, res) => {
+app.get('/download', async(req, res, err) => {
 	const v_id = req.query.url.split('v=')[1],
     info = await ytdl.getInfo(req.query.url);
-    // console.log(info.formats[4]);
-    // console.log(info.formats[1]);
-	res.render("download", {
-		url: "https://www.youtube.com/embed/" + v_id,
+	res.render('download', {
+		url: 'https://www.youtube.com/embed/' + v_id,
         info: info.formats.sort((a, b) => {
             return a.mimeType < b.mimeType;
         }),
 	});
 });
 
-app.get("/faqs", (req, res) => {
-	res.render("faqs");
+app.get('/faqs', (req, res) => {
+	res.render('faqs');
 });
 
 
